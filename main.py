e7eb8534866d6f1b314f6953765b965c6d8be923
misen <misenbai@gmail.com>
2023-08-09 18:14:22 +0600

    test1

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
-print('Hello World!')
+import os
+
+path = 'D:\\Python\\test'
+dirList = os.listdir(path)
+print(dirList)
+for i in dirList:
+    print(i,type(i),path+'\\'+i,os.path.isdir(path+'\\'+i))
