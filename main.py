aa9dbcc3dc154bb739ad6f237aab972293e19b9d
misen <misenbai@gmail.com>
2023-08-12 17:12:00 +0600

    update

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ def main():
               "3.Create folder\n "
               "4.Delete folder\n "
               "5.Folder list\n "
-              "6.Open folder\n "
+              "6.Replace file\n "
               "7.Quit")
         try:
             opp = int(input("Choose the operation: "))
@@ -31,7 +31,9 @@ def main():
                     folder_name = input("Which folder's entries do you want to get? ").strip(" ")
                     list_folder(folder_name)
                 case 6:
-                    replace_file()
+                    file_name = input("Give a file name: ").strip(" ")
+                    destination = input("Give folder name: ").strip(" ")
+                    replace_file(file_name, destination)
                 case 7:
                     print("Good bye!")
                     return False
@@ -59,6 +61,7 @@ def create_folder(folder_name):
     print("Folder had been created!")
 
 def delete_folder(folder_name):
+
     if is_empty(f"D:\\Python\\test\\{folder_name}"):
         os.rmdir(f"D:\\Python\\test\\{folder_name}")
         print("Folder removed.")
@@ -71,21 +74,48 @@ def delete_folder(folder_name):
             print("Folder is not removed.")
 
 def list_folder(folder_name):
-    if path.exists(f"D:\\Python\\test\\{folder_name}"):
-        if is_empty(f"D:\\Python\\test\\{folder_name}"):
-            print(f"Folder {folder_name} is empty.")
+    folder_path = find_folder("D:\\Python\\test", folder_name)
+
+    if folder_path:
+        if is_empty(folder_path):
+            print("Folder is empty.")
         else:
-            print(f"Here is the list in the folder {folder_name}")
-            for i in os.scandir(f"D:\\Python\\test\\{folder_name}"):
-                print(i.name)
+            print(f"Contents of folder '{folder_name}':")
+            for entry in os.scandir(folder_path):
+                print(entry.name)
     else:
-        print(f"Here is the list in the folder 'test'")
-        for i in os.scandir(f"D:\\Python\\test"):
-            print(i.name)
-def replace_file():
-    print("replace")
+        print(f"Folder '{folder_name}' not found.")
 
+def replace_file(file_name, destination):
+    folder_path = find_folder("D:\\Python\\test", destination)
+    file_path = find_file("D:\\Python\\test", file_name)
+    if folder_path and file_path:
+        shutil.move(file_path, folder_path)
+        print("File replaced successfully!")
+    else:
+        print(folder_path)
+        print(file_path)
+        print("The destination or file is not existing.")
 
+def find_folder(start_dir, folder_name):
+    for entry in os.scandir(start_dir):
+        if entry.is_dir() and entry.name == folder_name:
+            return entry.path
+        elif entry.is_dir():
+            result = find_folder(entry.path, folder_name)
+            if result:
+                return result
+    return None
+
+def find_file(start_dir, file_name):
+    for entry in os.scandir(start_dir):
+        if entry.is_file() and entry.name == f"{file_name}.txt":
+            return entry.path
+        elif entry.is_dir():
+            result = find_file(entry.path, file_name)
+            if result:
+                return result
+    return None
 def is_empty(folder_path):
     items = os.listdir(folder_path)
 
